2df3bc26beb03b92e750d7b52e2ac10ec393b057
Satya Prakash <satyaprakash@Satyas-MacBook-Pro.local>
2024-07-23 07:16:28 +0530

    added include additional directory option in props

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Function: BaseFunction, Code } = require('aws-cdk-lib/aws-lambda');
-const { mkdirSync, existsSync, copyFileSync, readFileSync, rmSync } = require('node:fs');
+const { mkdirSync, existsSync, copyFileSync, readFileSync, rmSync, cpSync } = require('node:fs');
 class Function extends BaseFunction {
     static libDir;
     static buildDir;
@@ -12,11 +12,30 @@ class Function extends BaseFunction {
         let packageName = fileName.replaceAll("/", "-");
         let packagePath = Function.buildDir + "/" + packageName;
         let directoryCreate = mkdirSync(packagePath, { recursive: true });
-        console.log('directoryCreate', directoryCreate);
-        console.log("packageName:", packageName);
+        //console.log('directoryCreate', directoryCreate);
+        //console.log("packageName:", packageName);
         rmSync(packagePath, { recursive: true });
         Function.createAllDependencyFiles(packagePath, props.code.path, fileName + ".mjs");
         props.code = Code.fromAsset(packagePath);
+        if (props.includeAdditionalDirectories) {
+
+            props.includeAdditionalDirectories.forEach(directory => {
+                mkdirSync(packagePath + "/" + directory, { recursive: true });
+                //console.log("create directory", packagePath+ "/" + directory);
+                cpSync(Function.libDir + "/" + directory, packagePath + "/" + directory, { recursive: true });
+            });
+
+            delete props.includeAdditionalDirectories;
+        }
+        if (props.includeAdditionalFiles) {
+            props.includeAdditionalFiles.forEach(file => {
+                let splitFile = file.split("/");
+                splitFile.pop();
+                mkdirSync(Function.buildDir + "/" + splitFile.join("/"), { recursive: true });
+                cpSync(Function.libDir + "/" + file, Function.buildDir + "/" + file, { recursive: true });
+            });
+            delete props.includeAdditionalFiles;
+        }
         super(scope, id, props);
     }
     static createDependenciesForTheImport = (packagePath, currentDir, importedFile) => {
@@ -39,11 +58,11 @@ class Function extends BaseFunction {
 
     }
     static createAllDependencyFiles = (packagePath, baseDir, fileName) => {
-        console.log(`createAllDependencyFiles`, packagePath, baseDir, fileName);
+        //console.log(`createAllDependencyFiles`, packagePath, baseDir, fileName);
         let lambdaDir = Function.libDir;
         let currentDir = baseDir + "/" + fileName.split("/").slice(0, -1).join("/");
         if (existsSync(baseDir.replace(lambdaDir, packagePath) + "/" + fileName)) {
-            console.log("file already created", baseDir.replace(lambdaDir, packagePath) + "/" + fileName);
+            //console.log("file already created", baseDir.replace(lambdaDir, packagePath) + "/" + fileName);
             return true;
         }
         mkdirSync(currentDir.replace(lambdaDir, packagePath), { recursive: true });
@@ -62,24 +81,24 @@ class Function extends BaseFunction {
         while ((match = es6Pattern.exec(fileData)) !== null) {
             importedFiles.push(match[1]);
             if (existsSync(currentDir + "/" + match[1])) {
-                console.log(`packagePath: ${packagePath}, currentDir: ${currentDir} , importedPackage: ${match[1]} `);
+                //console.log(`packagePath: ${packagePath}, currentDir: ${currentDir} , importedPackage: ${match[1]} `);
                 this.createDependenciesForTheImport(packagePath, currentDir, match[1]);
             }
             else {
-                console.log(`unmatched packagePath: ${packagePath}, currentDir: ${currentDir} , importedPackage: ${match[1]} `);
+                //console.log(`unmatched packagePath: ${packagePath}, currentDir: ${currentDir} , importedPackage: ${match[1]} `);
             }
         }
         while ((match = commonjsPattern.exec(fileData)) !== null) {
             if (existsSync(match[1])) {
-                console.log(`packagePath: ${packagePath}, currentDir: ${currentDir} , importedPackage: ${match[1]} `);
+                //console.log(`packagePath: ${packagePath}, currentDir: ${currentDir} , importedPackage: ${match[1]} `);
                 this.createDependenciesForTheImport(packagePath, currentDir, match[1]);
             }
             else {
-                console.log(`unmatched packagePath: ${packagePath}, currentDir: ${currentDir} , importedPackage: ${match[1]} `);
+                //console.log(`unmatched packagePath: ${packagePath}, currentDir: ${currentDir} , importedPackage: ${match[1]} `);
             }
         }
 
-        console.log(importedFiles);
+        //console.log(importedFiles);
 
     }
 }
